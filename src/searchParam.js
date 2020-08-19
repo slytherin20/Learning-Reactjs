65e0c7b7a93faabb82427b21f3b77a2846ebacf3
slytherin20 <sonaligupta2904@gmail.com>
2020-08-19 17:06:07 +0530

    Generic dropdown

--- src/searchParam.js
+++ src/searchParam.js
@@ -12,7 +12,7 @@ const SearchParams = () =>{
     <div className="search-params">
       <h1>{location}</h1>
         <form>
-          <label htmlFor=nt"location">
+          <label htmlFor="location">
             Location
             <input id="location" value={location} placeholder="location" onChange={e=>setLocation(e.target.value)}/>
           </label>
