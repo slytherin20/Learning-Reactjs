79b1086a508b43c346f3b440d4bf04a06dbad98c
slytherin20 <sonaligupta2904@gmail.com>
2020-08-19 19:21:19 +0530

    useEffect

--- src/searchParam.js
+++ src/searchParam.js
@@ -1,5 +1,5 @@
-import React,{ useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React,{ useState,useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from "./useDropdown";
 
 const SearchParams = () =>{
@@ -7,7 +7,18 @@ const SearchParams = () =>{
     const [location,setLocation] = useState("Seattle,WA");    //throws error
     const [breeds,setBreeds] = useState([]);
     const [animal,AnimalDropdown] = useDropdown("Animal","dog",ANIMALS);
-    const [breed,BreedDropdown] = useDropdown("Breed","",breeds);
+    const [breed,BreedDropdown,setBreed] = useDropdown("Breed","",breeds);
+
+    useEffect(() =>{
+      setBreeds([]);
+      setBreed("");
+      pet.breeds(animal).then(({breeds})=>{
+        const breedString = breeds.map(({name})=>name);
+        setBreeds(breedString);
+      },console.error)
+
+      },[animal,setBreeds,setBreed]);
+
   return (
     <div className="search-params">
       <h1>{location}</h1>
